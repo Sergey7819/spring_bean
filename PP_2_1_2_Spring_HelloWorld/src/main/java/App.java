05ea5db812fd02303ad2534bd6fa38fa31cb2ed2
Sergey7819 <socrat366@icloud.com>
2022-12-17 17:48:11 +0300

    added gitignore

--- PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
+++ PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
@@ -3,14 +3,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
 
-        HelloWorld bean1 =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean1.getMessage());
 
         Cat cat = (Cat) applicationContext.getBean("catBean");
@@ -19,10 +16,9 @@ public class App {
         Cat cat1 = (Cat) applicationContext.getBean("catBean");
         cat1.say();
 
-        System.out.println("Переменные ссылаются на один и тот же объект? " + (bean==bean1));
+        System.out.println("Переменные ссылаются на один и тот же объект? " + (bean == bean1));
 
-        System.out.println("Переменные ссылаются на один и тот же объект? " + (cat==cat1));
-
+        System.out.println("Переменные ссылаются на один и тот же объект? " + (cat == cat1));
 
 
     }
